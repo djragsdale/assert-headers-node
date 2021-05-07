8dadd7b5ed105b7d659389b5dba248181ffe95d9
David Ragsdale <me@davidjragsdale.com>
2021-05-07 16:10:52 -0400

    Add getConfiguration with tests

---
+++ bin/assert-headers/getConfiguration.js
@@ -0,0 +1,20 @@
+const { readFile } = require('fs')
+
+module.exports = function getConfiguration(configurationPath) {
+  return new Promise((resolve, reject) => {
+    try {
+      readFile(configurationPath, 'utf-8', (err, data) => {
+        if (err) return reject(err)
+
+        try {
+          const config = JSON.parse((data || '').toString())
+          resolve(config)
+        } catch (parseError) {
+          reject(parseError)
+        }
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
---
+++ bin/assert-headers/getConfiguration.spec.js
@@ -0,0 +1,22 @@
+/* global describe, expect, test */
+
+const path = require('path')
+
+const getConfiguration = require('./getConfiguration')
+
+describe('bin/assert-headers/getConfiguration', () => {
+  test('rejects if the file is missing', async () => {
+    await expect(getConfiguration(path.join(__dirname, './__fixtures__/nonexistent.json')))
+      .rejects.toThrow()
+  })
+  
+  test('rejects if the file can not be parsed', async () => {
+    await expect(getConfiguration(path.join(__dirname, './__fixtures__/badConfiguration.json')))
+      .rejects.toThrow()
+  })
+
+  test('resolves configuration from absolute path', async () => {
+    await expect(getConfiguration(path.join(__dirname, './__fixtures__/exampleConfiguration.json')))
+      .resolves.toBeInstanceOf(Object)
+  })
+})
